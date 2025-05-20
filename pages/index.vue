a820b25bee6a509bcef43c9e2298b5029c8660bb
gyarudos <k64.yuta29@gmail.com>
2025-05-20 19:28:47 +0900

    todo追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,29 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const count = ref(0)
+const text = ref('')
 
-const countUp = () => {
-  count.value++
+const toDo = ref<string[]>([])
+
+const addToDo = () => {
+  toDo.value.push(text.value)
+  text.value = ''
 }
 
 </script>
 
 <template>
-  <v-row class="ma-10 text-5xl" justify="center">
+  <!-- <v-row class="ma-10 text-5xl" justify="center">
     <p>
       {{ count }}
     </p>
-  </v-row>
-  <v-row justify="center">
-    <v-btn icon @click="countUp">
+  </v-row> -->
+  <v-row class="ma-10 text-5xl" justify="center">
+    <v-text-field v-model="text"></v-text-field>
+    <v-btn icon @click="addToDo">
       <v-icon>mdi-plus</v-icon>
     </v-btn>
   </v-row>
+  <v-card v-for="todo in toDo" :key="todo" :title="todo" class="mb-3 mx-3">
+  </v-card>
 </template>
