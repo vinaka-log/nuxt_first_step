d386761e51dd2cdd437121a2cb2d03f71ab01882
gyarudos <k64.yuta29@gmail.com>
2025-05-20 19:40:32 +0900

    画面遷移

---
+++ pages/complete.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const toDo = ref<string[]>([])
+
+</script>
+
+<template>
+  <p class="text-5xl">complete</p>
+  <v-card v-for="todo in toDo" :key="todo" :title="todo" class="mb-3 mx-3">
+  </v-card>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 
 const text = ref('')
 
@@ -10,6 +13,10 @@ const addToDo = () => {
   text.value = ''
 }
 
+const pushPage = () => {
+  router.push('/complete')
+}
+
 </script>
 
 <template>
@@ -18,12 +25,14 @@ const addToDo = () => {
       {{ count }}
     </p>
   </v-row> -->
-  <v-row class="ma-10 text-5xl" justify="center">
-    <v-text-field v-model="text"></v-text-field>
-    <v-btn icon @click="addToDo">
-      <v-icon>mdi-plus</v-icon>
+  <v-row class="ma-10" justify="center">
+    <v-btn icon rounded @click="pushPage">
+      完了済み
     </v-btn>
   </v-row>
-  <v-card v-for="todo in toDo" :key="todo" :title="todo" class="mb-3 mx-3">
-  </v-card>
+  <v-row class="ma-10 text-5xl" justify="center">
+    <v-text-field v-model="text"></v-text-field>
+    <v-card v-for="todo in toDo" :key="todo" :title="todo" class="mb-3 mx-3">
+    </v-card>
+  </v-row>
 </template>
